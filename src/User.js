a41390ea819bae7848720064e20e63d50dd45c64
prabhat.kuchibhotla <prabhat.kuchibhotla@servicenow.com>
2020-08-26 23:14:14 -0700

    Add follow button to User component. Handle onClick on Follow and add followingm2m if needed on DB.

--- src/User.js
+++ src/User.js
@@ -1,19 +1,61 @@
-import React from "react";
-import { Avatar } from "@material-ui/core";
+import React, { forwardRef } from "react";
+import { Avatar, Button } from "@material-ui/core";
 import "./User.css";
+import { useStateValue } from "./StateProvider";
+import db from "./firebase";
+import { actionTypes } from "./reducer";
 
-const User = ({ name, handle, profilePic }) => {
-  return (
-    <div className="user">
-      <Avatar src={profilePic} alt={name} className="profile__avatar" />
-      <div className="profile__name">
-        <h4>{name}</h4>
-        <span className="profile__handle">
-          <h5>{`@${handle}`}</h5>
-        </span>
+const User = forwardRef(
+  ({ name, handle, profilePic, displayFollowButton, id }, ref) => {
+    const [{ user, following }, dispatch] = useStateValue();
+
+    const handleFollow = (e) => {
+      e.preventDefault();
+      if (!followingUser()) {
+        db.collection("followingm2m")
+          .add({
+            follower: `users/${user.id}`,
+            following: id,
+          })
+          .then((docRef) => {
+            console.log("Followingm2m added with docId", docRef.id);
+            dispatch({
+              type: actionTypes.SET_FOLLOWING,
+              following: [...following, id],
+            });
+          })
+          .catch((error) => {
+            console.error("Failed while adding followingm2m", error);
+          });
+      }
+    };
+
+    const followingUser = () => {
+      return following.includes(id);
+    };
+
+    return (
+      <div className="user" ref={ref}>
+        <Avatar src={profilePic} alt={name} className="profile__avatar" />
+        <div className="profile__name">
+          <h4>{name}</h4>
+          <span className="profile__handle">
+            <h5>{`@${handle}`}</h5>
+          </span>
+        </div>
+        {displayFollowButton && (
+          <Button
+            className="user__followButton"
+            variant="contained"
+            type="submit"
+            onClick={handleFollow}
+          >
+            Follow
+          </Button>
+        )}
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default User;
